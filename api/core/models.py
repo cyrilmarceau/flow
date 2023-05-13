3be88e01ee4e682d27d9417a308ab172724f28d9
Cyril Marceau <cyril.marceau.dev@gmail.com>
2023-05-14 01:15:59 +0200

    feat(API): User model

--- api/core/models.py
+++ api/core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Custom user manager class"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a new user"""
+        if not email:
+            raise ValueError('User must have an email address')
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
