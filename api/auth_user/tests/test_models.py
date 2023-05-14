85f12e7f24e6a2609c97b72b1f98732c149e5985
Cyril Marceau <cyril.marceau.dev@gmail.com>
2023-05-14 22:26:15 +0200

    test(API): Raise error if email are invalid

--- api/auth_user/tests/test_models.py
+++ api/auth_user/tests/test_models.py
@@ -32,3 +32,9 @@ class ModelTests(TestCase):
             user = get_user_model().objects.create_user(email, 'password12345')
 
             self.assertEqual(user.email, expected)
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'password12345')
