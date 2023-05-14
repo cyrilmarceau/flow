499cac9681d54123f6baf4f98356cc57641ff684
Cyril Marceau <cyril.marceau.dev@gmail.com>
2023-05-14 22:33:13 +0200

    test(API): Create superuser

--- api/auth_user/tests/test_models.py
+++ api/auth_user/tests/test_models.py
@@ -38,3 +38,15 @@ class ModelTests(TestCase):
 
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'password12345')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+
+        user = get_user_model().objects.create_superuser(
+            'cyril@example.com',
+            'password12345'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+        
